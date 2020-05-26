adb931065ca6bceee26c4f298298dc39e5ec030d
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-26 21:59:55 +0530

    model user completed

---
+++ src/main/java/in/himanshugawari/reddit/model/User.java
@@ -0,0 +1,42 @@
+package in.himanshugawari.reddit.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	
+	@NotBlank(message = "Username is required")
+	private String userName;
+	
+	@NotBlank(message = "Password is required")
+	private String password;
+	
+	@Email
+	@NotEmpty(message = "Email is required")
+	private String email;
+	
+	private Instant userCreated;
+	
+	private Boolean enabled;
+}
