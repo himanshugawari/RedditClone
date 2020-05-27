3ea3630a47d70d867e84c80336d4a1e0c34d1efb
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-27 21:08:53 +0530

    repo subreddit repo added

---
+++ src/main/java/in/himanshugawari/reddit/repository/SubredditRepository.java
@@ -0,0 +1,11 @@
+package in.himanshugawari.reddit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import in.himanshugawari.reddit.model.Subreddit;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+
+}
