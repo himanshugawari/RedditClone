faf8eb7035979b6832cb082b4f9fda9d164f7e85
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-27 22:16:51 +0530

    added exception for h2-console

---
+++ src/main/java/in/himanshugawari/reddit/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package in.himanshugawari.reddit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**")
+				.permitAll();
+	}
+}
