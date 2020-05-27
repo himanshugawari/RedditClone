b04068130ff02652ccef6e98d365390f401d6f57
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-27 21:09:34 +0530

    repo user repo added

---
+++ src/main/java/in/himanshugawari/reddit/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package in.himanshugawari.reddit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import in.himanshugawari.reddit.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
