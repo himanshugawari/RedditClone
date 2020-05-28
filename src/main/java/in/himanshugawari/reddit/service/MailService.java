4356d887d0bd27bbf4e9883fe983bfdaf1ae1461
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-28 13:51:56 +0530

    mail service added

---
+++ src/main/java/in/himanshugawari/reddit/service/MailService.java
@@ -0,0 +1,41 @@
+package in.himanshugawari.reddit.service;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import in.himanshugawari.reddit.exceptions.SpringRedditException;
+import in.himanshugawari.reddit.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+	private final JavaMailSender javaMailSender;
+	private final MailContentBuilder mailContentBuilder;
+
+	@Async
+	void sendMail(NotificationEmail notificationEmail) throws SpringRedditException {
+		MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
+			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+			mimeMessageHelper.setFrom("himanshu.email.helper@gmail.com");
+			mimeMessageHelper.setTo(notificationEmail.getRecipient());
+			mimeMessageHelper.setSubject(notificationEmail.getSubject());
+			mimeMessageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+			// mimeMessageHelper.setText(notificationEmail.getBody());
+		};
+		try {
+			javaMailSender.send(mimeMessagePreparator);
+			log.info("Activation mail sent!!!");
+		} catch (Exception e) {
+			log.error("Exception occured while sending mail", e);
+			throw new SpringRedditException(
+					"Exception occured while sending mail to " + notificationEmail.getRecipient(), e);
+		}
+	}
+}
