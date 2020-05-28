bcda300e40a71e279fbb416b451dd031526fa996
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-28 12:31:38 +0530

    added BCryptPasswordEncoder

--- src/main/java/in/himanshugawari/reddit/config/SecurityConfiguration.java
+++ src/main/java/in/himanshugawari/reddit/config/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package in.himanshugawari.reddit.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +18,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.headers().frameOptions().disable();
 		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/h2-console/**").permitAll();
 		//http.authorizeRequests().antMatchers("/api/auth/**").permitAll().anyRequest().authenticated();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+		
 	}
 }
