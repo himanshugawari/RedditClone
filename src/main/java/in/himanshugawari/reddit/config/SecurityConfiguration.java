f09d0b7f77a3331ef2a6fa0544ceb24bc702f86e
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-28 12:12:43 +0530

    configure method updated

--- src/main/java/in/himanshugawari/reddit/config/SecurityConfiguration.java
+++ src/main/java/in/himanshugawari/reddit/config/SecurityConfiguration.java
@@ -2,16 +2,18 @@ package in.himanshugawari.reddit.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
-		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/h2-console/**")
-				.permitAll();
+		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/h2-console/**").permitAll();
+		//http.authorizeRequests().antMatchers("/api/auth/**").permitAll().anyRequest().authenticated();
 	}
 }
