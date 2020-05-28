429f3be70d8f4d6ccb63ca9021ada8df88a715b8
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-28 12:34:32 +0530

    added allargsconstructor, final , transcational. removed autowired

--- src/main/java/in/himanshugawari/reddit/service/AuthService.java
+++ src/main/java/in/himanshugawari/reddit/service/AuthService.java
@@ -2,25 +2,29 @@ package in.himanshugawari.reddit.service;
 
 import java.time.Instant;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.transaction.Transactional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import in.himanshugawari.reddit.dto.RegisterRequest;
 import in.himanshugawari.reddit.model.User;
 import in.himanshugawari.reddit.repository.UserRepository;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	@Autowired
-	private UserRepository userRepository;
-	
+
+	// @Autowired
+	private final PasswordEncoder passwordEncoder;
+
+	// @Autowired
+	private final UserRepository userRepository;
+
+	@Transactional
 	public void signup(RegisterRequest registerRequest) {
-		User user=new User();
+		User user = new User();
 		user.setUserName(registerRequest.getUsername());
 		user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
 		user.setEmail(registerRequest.getEmail());
