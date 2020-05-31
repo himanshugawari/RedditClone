9d0e62e32d647662a5b7bc6e55cead9e89e9524a
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-31 19:48:00 +0530

    added AuthenticationManager , configureGlobal

--- src/main/java/in/himanshugawari/reddit/config/SecurityConfiguration.java
+++ src/main/java/in/himanshugawari/reddit/config/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package in.himanshugawari.reddit.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,25 +19,33 @@ import lombok.AllArgsConstructor;
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	private final UserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
-		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/h2-console/**").permitAll();
-		//http.authorizeRequests().antMatchers("/api/auth/**").permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/h2-console/**")
+				.permitAll();
+		// http.authorizeRequests().antMatchers("/api/auth/**").permitAll().anyRequest().authenticated();
 	}
-	
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-	
+
+	// @Bean(initMethod = BeanIds.AUTHENTICATION_MANAGER)
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-		
+
 	}
 }
