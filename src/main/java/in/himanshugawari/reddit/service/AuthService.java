3f613adf54e603c06ec733554fa381c9d1b0ae3e
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-31 10:33:24 +0530

    added send mail method

--- src/main/java/in/himanshugawari/reddit/service/AuthService.java
+++ src/main/java/in/himanshugawari/reddit/service/AuthService.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import in.himanshugawari.reddit.dto.RegisterRequest;
+import in.himanshugawari.reddit.model.NotificationEmail;
 import in.himanshugawari.reddit.model.User;
 import in.himanshugawari.reddit.model.VerificationToken;
 import in.himanshugawari.reddit.repository.UserRepository;
@@ -21,12 +22,10 @@ public class AuthService {
 
 	// @Autowired
 	private final PasswordEncoder passwordEncoder;
-
-	// @Autowired
 	private final UserRepository userRepository;
-	
 	private final VerificationTokenRepository verificationTokenRepository;
-
+	private final MailService mailService;
+	
 	@Transactional
 	public void signup(RegisterRequest registerRequest) {
 		User user = new User();
@@ -38,6 +37,9 @@ public class AuthService {
 		userRepository.save(user);
 		
 		String token=generateVerificationToken(user);
+		mailService.sendMail(new NotificationEmail("Please Activate your account", user.getEmail(), "Thank you for signing up to Spring Reddit, " +
+                "please click on the below url to activate your account : " +
+                "http://localhost:8080/api/auth/accountVerification/" + token));
 	}
 	
 	private String generateVerificationToken(User user) {
