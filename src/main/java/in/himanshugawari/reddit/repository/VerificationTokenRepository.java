9ca5ca93669ea6b154fb0f35ba034a3b44df97ff
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-31 14:01:36 +0530

    added findByToken

--- src/main/java/in/himanshugawari/reddit/repository/VerificationTokenRepository.java
+++ src/main/java/in/himanshugawari/reddit/repository/VerificationTokenRepository.java
@@ -1,5 +1,7 @@
 package in.himanshugawari.reddit.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import in.himanshugawari.reddit.model.VerificationToken;
 
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+	Optional<VerificationToken> findByToken(String token);
 
 }
