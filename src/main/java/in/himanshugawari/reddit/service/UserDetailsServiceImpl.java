e2f78ab3a2cfae7ae8e72b2fea104bfd234b6317
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-31 18:32:27 +0530

    added UserDetailsServiceImpl for UserDetailsService

---
+++ src/main/java/in/himanshugawari/reddit/service/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package in.himanshugawari.reddit.service;
+
+import static java.util.Collections.singletonList;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import in.himanshugawari.reddit.model.User;
+import in.himanshugawari.reddit.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> userOptional = userRepository.findByUserName(username);
+		User user = userOptional
+				.orElseThrow(() -> new UsernameNotFoundException("No user found with username - " + username));
+
+		return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(),
+				user.getEnabled(), true, true, true, getAuthorities("USER"));
+	}
+
+	private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+		return singletonList(new SimpleGrantedAuthority(role));
+	}
+
+}
