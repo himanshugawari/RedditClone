39d75ef7186c8d9d3193eec246183d35de4a472f
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-31 10:34:05 +0530

    added AuthService in singnup

--- src/main/java/in/himanshugawari/reddit/controller/AuthController.java
+++ src/main/java/in/himanshugawari/reddit/controller/AuthController.java
@@ -1,19 +1,27 @@
 package in.himanshugawari.reddit.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.himanshugawari.reddit.dto.RegisterRequest;
+import in.himanshugawari.reddit.service.AuthService;
+import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping(path = {"/api/auth/"})
+@RequestMapping(path = { "/api/auth/" })
+@AllArgsConstructor
 public class AuthController {
-	
-	@PostMapping(path = {"/signup"})
-	public void signup(@RequestBody RegisterRequest registerRequest) {
-		
+
+	private final AuthService authService;
+
+	@PostMapping(path = { "/signup" })
+	public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+		authService.signup(registerRequest);
+		return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
 	}
 
 }
