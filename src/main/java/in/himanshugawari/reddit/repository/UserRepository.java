b14d449fe3215bb953256ff19040ed7072613258
Himanshu Gawari <himanshu25031991@gmail.com>
2020-05-31 14:01:14 +0530

    added findByUserName

--- src/main/java/in/himanshugawari/reddit/repository/UserRepository.java
+++ src/main/java/in/himanshugawari/reddit/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package in.himanshugawari.reddit.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import in.himanshugawari.reddit.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUserName(String username);
 
 }
