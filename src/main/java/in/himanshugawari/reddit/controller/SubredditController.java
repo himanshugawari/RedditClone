18335d6355b7a5ec0a34e5f33d886f68111217fa
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-01 17:08:54 +0530

    added getAllSubreddits method

--- src/main/java/in/himanshugawari/reddit/controller/SubredditController.java
+++ src/main/java/in/himanshugawari/reddit/controller/SubredditController.java
@@ -2,6 +2,7 @@ package in.himanshugawari.reddit.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +23,10 @@ public class SubredditController {
 	@PostMapping
 	public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(subredditService.save(subredditDto));
+	}
+	
+	@GetMapping
+	public void getAllSubreddits() {
+		subredditService.getAll();		
 	}
 }
