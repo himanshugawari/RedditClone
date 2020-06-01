b093874cc72edf01eed5335a53978c585a9a317f
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-01 16:53:16 +0530

    created SubredditDto

---
+++ src/main/java/in/himanshugawari/reddit/dto/SubredditDto.java
@@ -0,0 +1,16 @@
+package in.himanshugawari.reddit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubredditDto {
+	private Long id;
+	private String name;
+	private String description;
+	private Integer numberOfPosts;
+
+}
