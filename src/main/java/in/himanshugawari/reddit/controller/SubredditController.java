6454a6ab4a77e0a5f9ec9bc7731ad12558616776
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-01 17:06:01 +0530

    added createSubreddit method

--- src/main/java/in/himanshugawari/reddit/controller/SubredditController.java
+++ src/main/java/in/himanshugawari/reddit/controller/SubredditController.java
@@ -1,22 +1,26 @@
 package in.himanshugawari.reddit.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.himanshugawari.reddit.dto.SubredditDto;
+import in.himanshugawari.reddit.service.SubredditService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(path = { "/api/subreddit" })
 @AllArgsConstructor
-@Slf4j
+//@Slf4j
 public class SubredditController {
 
-	@PostMapping
-	public void createSubreddit(@RequestBody SubredditDto subredditDto) {
+	private final SubredditService subredditService;
 
+	@PostMapping
+	public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(subredditService.save(subredditDto));
 	}
 }
