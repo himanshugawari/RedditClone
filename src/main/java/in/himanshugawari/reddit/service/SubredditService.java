39af3f785b68436a2504993e18534e486f0c1455
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-01 17:35:12 +0530

    added getAll and mapToDto methods

--- src/main/java/in/himanshugawari/reddit/service/SubredditService.java
+++ src/main/java/in/himanshugawari/reddit/service/SubredditService.java
@@ -1,6 +1,10 @@
 package in.himanshugawari.reddit.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import in.himanshugawari.reddit.dto.SubredditDto;
 import in.himanshugawari.reddit.model.Subreddit;
@@ -14,6 +18,7 @@ public class SubredditService {
 
 	private final SubredditRepository subredditRepository;
 
+	@Transactional
 	public SubredditDto save(SubredditDto subredditDto) {
 		Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
 		subredditDto.setId(save.getId());
@@ -22,5 +27,15 @@ public class SubredditService {
 
 	private Subreddit mapSubredditDto(SubredditDto subredditDto) {
 		return Subreddit.builder().name(subredditDto.getName()).description(subredditDto.getDescription()).build();
+	}
+
+	@Transactional(readOnly = true)
+	public List<SubredditDto> getAll() {
+		return subredditRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+	}
+
+	private SubredditDto mapToDto(Subreddit subreddit) {
+		return SubredditDto.builder().name(subreddit.getName()).id(subreddit.getId())
+				.numberOfPosts(subreddit.getPosts().size()).build();
 	}
 }
