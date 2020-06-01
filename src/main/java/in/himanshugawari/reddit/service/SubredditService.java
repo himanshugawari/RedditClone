4472b95ce569e65f7cd2e4ce1087e8ae496593fd
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-01 16:54:48 +0530

    created SubredditService

---
+++ src/main/java/in/himanshugawari/reddit/service/SubredditService.java
@@ -0,0 +1,13 @@
+package in.himanshugawari.reddit.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class SubredditService {
+	
+}
