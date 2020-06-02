a12e3c5486d87d4b3fc51aad10d573b9005615ac
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-02 19:37:03 +0530

    SubredditDto mapSubredditToDto

--- src/main/java/in/himanshugawari/reddit/mapper/SubredditMapper.java
+++ src/main/java/in/himanshugawari/reddit/mapper/SubredditMapper.java
@@ -1,8 +1,16 @@
 package in.himanshugawari.reddit.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import in.himanshugawari.reddit.dto.SubredditDto;
+import in.himanshugawari.reddit.model.Subreddit;
 
 @Mapper(componentModel = "spring")
 public interface SubredditMapper {
 
+	@Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+	SubredditDto mapSubredditToDto(Subreddit subreddit);
+	
+	
 }
