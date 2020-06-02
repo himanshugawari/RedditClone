f4368a1d378d1758306a093c732429130559d7e4
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-02 20:36:18 +0530

    removed normal mapping and added mapstruct mapping

--- src/main/java/in/himanshugawari/reddit/service/SubredditService.java
+++ src/main/java/in/himanshugawari/reddit/service/SubredditService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import in.himanshugawari.reddit.dto.SubredditDto;
+import in.himanshugawari.reddit.exceptions.SpringRedditException;
+import in.himanshugawari.reddit.mapper.SubredditMapper;
 import in.himanshugawari.reddit.model.Subreddit;
 import in.himanshugawari.reddit.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
@@ -17,25 +19,40 @@ import lombok.AllArgsConstructor;
 public class SubredditService {
 
 	private final SubredditRepository subredditRepository;
+	private final SubredditMapper subredditMapper;
 
 	@Transactional
 	public SubredditDto save(SubredditDto subredditDto) {
-		Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
+		// Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
+		Subreddit save = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
 		subredditDto.setId(save.getId());
 		return subredditDto;
 	}
 
-	private Subreddit mapSubredditDto(SubredditDto subredditDto) {
-		return Subreddit.builder().name(subredditDto.getName()).description(subredditDto.getDescription()).build();
-	}
-
 	@Transactional(readOnly = true)
 	public List<SubredditDto> getAll() {
-		return subredditRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+		// return
+		// subredditRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+		return subredditRepository.findAll().stream().map(subredditMapper::mapSubredditToDto)
+				.collect(Collectors.toList());
 	}
 
-	private SubredditDto mapToDto(Subreddit subreddit) {
-		return SubredditDto.builder().name(subreddit.getName()).id(subreddit.getId())
-				.numberOfPosts(subreddit.getPosts().size()).build();
+	/*
+	public SubredditDto getSubreddit(Long id) {
+		Subreddit subreddit = subredditRepository.findById(id)
+				.orElseThrow(() -> new SpringRedditException("No subreddit found with ID - " + id));
+		return subredditMapper.mapSubredditToDto(subreddit);
 	}
+*/
+	
+	// changing from normal to mapstruct mapper
+	/*
+	 * private SubredditDto mapToDto(Subreddit subreddit) { return
+	 * SubredditDto.builder().name(subreddit.getName()).id(subreddit.getId())
+	 * .numberOfPosts(subreddit.getPosts().size()).build(); }
+	 * 
+	 * private Subreddit mapSubredditDto(SubredditDto subredditDto) { return
+	 * Subreddit.builder().name(subredditDto.getName()).description(subredditDto.
+	 * getDescription()).build(); }
+	 */
 }
