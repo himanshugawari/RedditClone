a94dc3a5345f52247fa9a52e95f45ca36c1fa367
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-02 19:39:04 +0530

    Integer mapPosts

--- src/main/java/in/himanshugawari/reddit/mapper/SubredditMapper.java
+++ src/main/java/in/himanshugawari/reddit/mapper/SubredditMapper.java
@@ -1,9 +1,12 @@
 package in.himanshugawari.reddit.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import in.himanshugawari.reddit.dto.SubredditDto;
+import in.himanshugawari.reddit.model.Post;
 import in.himanshugawari.reddit.model.Subreddit;
 
 @Mapper(componentModel = "spring")
@@ -12,5 +15,7 @@ public interface SubredditMapper {
 	@Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
 	SubredditDto mapSubredditToDto(Subreddit subreddit);
 	
-	
+	default Integer mapPosts(List<Post> numberOfPosts) {
+		return numberOfPosts.size();
+	}
 }
