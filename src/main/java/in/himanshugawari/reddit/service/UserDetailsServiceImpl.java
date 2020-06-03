d56e7a5dea54f1afff7e51d920c8d074ced296aa
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-03 22:40:00 +0530

    PostService

--- src/main/java/in/himanshugawari/reddit/service/UserDetailsServiceImpl.java
+++ src/main/java/in/himanshugawari/reddit/service/UserDetailsServiceImpl.java
@@ -26,12 +26,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	@Transactional(readOnly = true)
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<User> userOptional = userRepository.findByUserName(username);
+		Optional<User> userOptional = userRepository.findByUsername(username);
 		User user = userOptional
 				.orElseThrow(() -> new UsernameNotFoundException("No user found with username - " + username));
 
-		return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(),
-				user.getEnabled(), true, true, true, getAuthorities("USER"));
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				user.isEnabled(), true, true, true, getAuthorities("USER"));
 	}
 
 	private Collection<? extends GrantedAuthority> getAuthorities(String role) {
