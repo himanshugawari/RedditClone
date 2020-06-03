2cec11063133e9ea16cbc71fa073b48caa58cd21
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-03 22:37:00 +0530

    updated

--- src/main/java/in/himanshugawari/reddit/repository/PostRepository.java
+++ src/main/java/in/himanshugawari/reddit/repository/PostRepository.java
@@ -1,11 +1,18 @@
 package in.himanshugawari.reddit.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import in.himanshugawari.reddit.model.Post;
+import in.himanshugawari.reddit.model.Subreddit;
+import in.himanshugawari.reddit.model.User;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+	List<Post> findAllBySubreddit(Subreddit subreddit);
+
+	List<Post> findByUser(User user);
 }
