0b86eaf444c256f007e7d9aec4307cbdb34f5868
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-03 22:22:07 +0530

    added AuthenticationResponse

--- src/main/java/in/himanshugawari/reddit/dto/AuthenticationResponse.java
+++ src/main/java/in/himanshugawari/reddit/dto/AuthenticationResponse.java
@@ -1,13 +1,17 @@
 package in.himanshugawari.reddit.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AuthenticationResponse {
 	private String authenticationToken;
+	// private String refreshToken;
+	// private Instant expiresAt;
 	private String username;
 }
