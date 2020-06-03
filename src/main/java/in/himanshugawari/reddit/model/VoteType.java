9ed89b7aa9823d28c647ad06204cdcef94b7ac58
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-03 22:29:57 +0530

    updated

--- src/main/java/in/himanshugawari/reddit/model/VoteType.java
+++ src/main/java/in/himanshugawari/reddit/model/VoteType.java
@@ -1,9 +1,24 @@
 package in.himanshugawari.reddit.model;
 
+import java.util.Arrays;
+
+import in.himanshugawari.reddit.exceptions.SpringRedditException;
+
 public enum VoteType {
-	UPVOTE(1),DOWNVOTE(-1);
-	
-	private VoteType(int direction) {
-		
+
+	UPVOTE(1), DOWNVOTE(-1);
+
+	private int direction;
+
+	VoteType(int direction) {
+	}
+
+	public static VoteType lookup(Integer direction) {
+		return Arrays.stream(VoteType.values()).filter(value -> value.getDirection().equals(direction)).findAny()
+				.orElseThrow(() -> new SpringRedditException("Vote not found"));
+	}
+
+	public Integer getDirection() {
+		return direction;
 	}
 }
