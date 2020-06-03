f0e0c545bcf32fd9508274747a90f99eba3b0637
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-03 22:37:43 +0530

    updated

--- src/main/java/in/himanshugawari/reddit/repository/VoteRepository.java
+++ src/main/java/in/himanshugawari/reddit/repository/VoteRepository.java
@@ -1,11 +1,16 @@
 package in.himanshugawari.reddit.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import in.himanshugawari.reddit.model.Post;
+import in.himanshugawari.reddit.model.User;
 import in.himanshugawari.reddit.model.Vote;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
+	Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
