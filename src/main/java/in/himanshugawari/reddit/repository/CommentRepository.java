f11818dc471001a1ab87bd8bc1c9a47754d1331e
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-03 22:36:41 +0530

    added CommentRepository

--- src/main/java/in/himanshugawari/reddit/repository/CommentRepository.java
+++ src/main/java/in/himanshugawari/reddit/repository/CommentRepository.java
@@ -1,11 +1,18 @@
 package in.himanshugawari.reddit.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import in.himanshugawari.reddit.model.Comment;
+import in.himanshugawari.reddit.model.Post;
+import in.himanshugawari.reddit.model.User;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+	List<Comment> findByPost(Post post);
+
+	List<Comment> findAllByUser(User user);
 }
