7a811dc52408dedc4cb00f0111efe86f09067ad5
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-04 19:23:53 +0530

    added getAllCommentsForPost and getAllCommentsForUser controller

--- src/main/java/in/himanshugawari/reddit/controller/CommentsController.java
+++ src/main/java/in/himanshugawari/reddit/controller/CommentsController.java
@@ -1,7 +1,11 @@
 package in.himanshugawari.reddit.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +25,16 @@ public class CommentsController {
 	@PostMapping
 	public ResponseEntity<CommentsDto> createComment(@RequestBody CommentsDto commentsDto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(commentService.save(commentsDto));
+	}
+
+	@GetMapping("/by-post/{postId}")
+	public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@PathVariable Long postId) {
+		return ResponseEntity.status(HttpStatus.OK).body(commentService.getAllCommentsForPost(postId));
+	}
+
+	@GetMapping("/by-user/{userName}")
+	public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@PathVariable String userName) {
+		return ResponseEntity.status(HttpStatus.OK).body(commentService.getAllCommentsForUser(userName));
 	}
 
 }
