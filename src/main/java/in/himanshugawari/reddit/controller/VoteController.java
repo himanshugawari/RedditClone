49ae885f52f2c22b38d81496d1ebd6e9557eda8e
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-04 20:28:55 +0530

    update

--- src/main/java/in/himanshugawari/reddit/controller/VoteController.java
+++ src/main/java/in/himanshugawari/reddit/controller/VoteController.java
@@ -1,5 +1,33 @@
 package in.himanshugawari.reddit.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.himanshugawari.reddit.dto.VoteDto;
+import in.himanshugawari.reddit.model.Vote;
+import in.himanshugawari.reddit.service.VoteService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/votes/")
+@AllArgsConstructor
 public class VoteController {
 
+	private final VoteService voteService;
+
+	/*
+	 * @PostMapping public ResponseEntity<Void> vote(@RequestBody VoteDto voteDto) {
+	 * voteService.vote(voteDto); return new ResponseEntity<>(HttpStatus.OK); }
+	 */
+
+	@PostMapping
+	public ResponseEntity<Optional<Vote>> vote(@RequestBody VoteDto voteDto) {
+		return ResponseEntity.status(HttpStatus.OK).body(voteService.vote(voteDto));
+	}
 }
