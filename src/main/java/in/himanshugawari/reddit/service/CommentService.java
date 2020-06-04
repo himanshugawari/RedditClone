2bc4d06e3b884136e8e9f259542c4a7ec86c6681
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-04 18:55:21 +0530

    added CommentService

---
+++ src/main/java/in/himanshugawari/reddit/service/CommentService.java
@@ -0,0 +1,14 @@
+package in.himanshugawari.reddit.service;
+
+import org.springframework.stereotype.Service;
+
+import in.himanshugawari.reddit.dto.CommentsDto;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+	public void save(CommentsDto commentsDto) {
+
+	}
+}
