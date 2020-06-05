103321bd8e3fbe1ecd6610f5187336cb3b62247e
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-05 12:37:03 +0530

    import SwaggerConfiguration.class

--- src/main/java/in/himanshugawari/reddit/RedditApplication.java
+++ src/main/java/in/himanshugawari/reddit/RedditApplication.java
@@ -2,10 +2,14 @@ package in.himanshugawari.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import in.himanshugawari.reddit.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(value = { SwaggerConfiguration.class })
 public class RedditApplication {
 
 	public static void main(String[] args) {
