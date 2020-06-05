a8c61a6ed03221d366bfb32c65a2847845833c7f
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-05 13:29:50 +0530

    added anotations for junit4

--- src/test/java/in/himanshugawari/reddit/RedditApplicationTests.java
+++ src/test/java/in/himanshugawari/reddit/RedditApplicationTests.java
@@ -1,8 +1,11 @@
 package in.himanshugawari.reddit;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(value = SpringRunner.class)
 @SpringBootTest
 class RedditApplicationTests {
 
